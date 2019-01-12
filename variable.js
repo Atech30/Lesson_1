ff85062546a7890d78126461938304d00b61eccc
Abdul bobsonkabia kabia <a_kabia@yahoo.com>
2019-01-12 14:51:14 -0500

    My first commit

---
+++ variable.js
@@ -0,0 +1,10 @@
+let myAge =21;
+console.log(myAge);
+// myAge = 45;
+console.log(myAge);
+ 
+ const firstName="Abdul";
+ const lastName="kabia";
+ let greeting="hello my name is ";
+ console.log(greeting + lastName);
+ greeting="i am " + myAge + " years old";
